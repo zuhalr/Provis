82c04f2cf9906456c7930441a5af20d04b8e7533
zuhalr <robbani.zuhal@gmail.com>
2022-04-07 21:08:40 +0700

    Create addprestasi.dart

---
+++ lib/addprestasi.dart
@@ -0,0 +1,116 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:provis/Widgets/CustomCard.dart';
+import 'package:provis/Widgets/theme.dart';
+import 'package:provis/profil_dosen.dart';
+
+import 'daftarfakultas.dart';
+import 'list_dosen.dart';
+import 'fasilitas.dart';
+import 'profil_dosen.dart';
+import 'home_view.dart';
+
+class Compare extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 143, 5, 5),
+        title: Center(
+          child: Text("Compare",
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)),
+        ),
+        bottom: AppBar(
+          backgroundColor: Colors.white,
+          title: Container(
+            width: double.infinity,
+            height: 40,
+            color: Colors.white,
+            child: Center(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Cari',
+                  prefixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    tooltip: 'search',
+                    onPressed: () {
+                      // kalau ditap
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: [
+          CustomCard(title: "FPMIPA", image: "assets/images/fpmipa.jpg"),
+          CustomCard(title: "FIP", image: "assets/images/FIP.jpg"),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: primary,
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              //Daftar Fakultas
+              child: IconButton(
+                color: colorLight,
+                icon: Icon(Icons.home_work),
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return Daftarfakultas();
+                  }));
+                },
+              ),
+            ),
+            Expanded(
+              //Dashboard
+              child: IconButton(
+                color: colorLight,
+                icon: Icon(Icons.show_chart),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              //Home
+              child: IconButton(
+                color: colorLight,
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return HomePage();
+                  }));
+                },
+              ),
+            ),
+            Expanded(
+              //Menu Compare
+              child: IconButton(
+                color: colorLight,
+                icon: Icon(Icons.compare),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              //Menu kaprodi (Edit Data Prodi)
+              child: IconButton(
+                color: colorLight,
+                icon: Icon(Icons.person),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
