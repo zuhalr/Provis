7ef69b2320e09d72d1d0512d67fb9878ddf210f7
zuhalr <robbani.zuhal@gmail.com>
2022-04-08 08:31:42 +0700

    Update compare.dart

--- lib/compare.dart
+++ lib/compare.dart
@@ -48,10 +48,304 @@ class Compare extends StatelessWidget {
       ),
       body: Center(
         child: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(2),
+          crossAxisSpacing: 1,
+          mainAxisSpacing: 1,
           crossAxisCount: 2,
+          childAspectRatio: (1 / .2),
           children: [
-            CustomCard(title: "FPMIPA", image: "assets/images/fpmipa.jpg"),
-            CustomCard(title: "FIP", image: "assets/images/FIP.jpg"),
+            // CustomCard(title: "FPMIPA", image: "assets/images/fpmipa.jpg"),
+            // CustomCard(title: "FIP", image: "assets/images/FIP.jpg"),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Nama Fakultas",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Nama Fakultas",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Fakultas Ilmu Pendidikan'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text(
+                  'Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Status",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Status",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Aktif'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('Aktif'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Akreditasi",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Akreditasi",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('A'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('A'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Jumlah Prodi",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Jumlah Prodi",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('9 Prodi'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('12 Prodi'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Rasio Dosen/Mahasiswa",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Rasio Dosen/Mahasiswa",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('1 : 10'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('1 : 18'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Rata-Rata Waktu Kelulusan",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              height: 80,
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                //20 pixel ke semua arah
+                child: ElevatedButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Rata-Rata Waktu Kelulusan",
+                        style: heading5,
+                      ),
+                    ),
+                    onPressed: () {}),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('4 Tahun'),
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Text('3.8 Tahun'),
+              alignment: Alignment.center,
+            ),
           ],
         ),
       ),
