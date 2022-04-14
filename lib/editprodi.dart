990c6d7952a07a4dece7ac1f922cde3030245172
samuelnelson1000 <samuelnelson_1901584@upi.edu>
2022-04-14 18:22:44 +0700

    Revert "Revert "tambah daftarprodi dan editprodi""
    This reverts commit 0b6fa526da13ed1fda7ab00cfdaf06041549f9a0.

---
+++ lib/editprodi.dart
@@ -0,0 +1,165 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:provis/Widgets/CustomCard.dart';
+import 'package:provis/Widgets/theme.dart';
+import 'package:provis/profil_dosen.dart';
+import 'package:provis/compare.dart';
+import 'daftarfakultas.dart';
+import 'list_dosen.dart';
+import 'fasilitas.dart';
+import 'profil_dosen.dart';
+import 'home_view.dart';
+
+class EditProdi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 143, 5, 5),
+        title: Center(
+          child: Text("Edit Prodi",
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)),
+        ),
+      ),
+      backgroundColor: colorLight,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(24, 40, 24, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                  child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Jumlah Mahasiswa',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Jumlah Dosen',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Akreditasi',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Jenjang',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Rata-rata Waktu Kelulusan',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: 'Riwayat Kerjasama',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border:
+                              OutlineInputBorder(borderSide: BorderSide.none)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                ],
+              )),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 80,
+                width: 500,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  //20 pixel ke semua arah
+                  child: ElevatedButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 143, 5, 5)),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Update",
+                          style: heading5,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return HomePage();
+                        }));
+                      }),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
